6394855868c4b70ba1caa26f4706a7a5e76c2aa6
VICKI PAN (ITA-ISDC-ISD-OOCLL/ZHA) <PANVI@OOCL.COM>
2020-07-22 20:55:11 +0800

    Vicki -- function submission -- complete pracce08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,31 @@
 package practice08;
 
 public class Klass {
+    protected int number;
+    protected Student leader;
+    public Klass(int number){
+        this.number = number;
+    }
+    public int getNumber() {
+        return number;
+    }
+    public String getDisplayName(){
+        return String.format("Class %s",this.number);
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public Student assignLeader(Student student){
+        this.leader = student;
+        return leader;
+    }
 }
--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,54 @@
 package practice08;
 
+import java.util.Objects;
+
 public class Person {
+    protected String name;
+    protected int age;
+    protected int id;
+    public Person(int id,String personName,int personAge){
+        this.name = personName;
+        this.age = personAge;
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String introduce(){
+        return String.format("My name is %s. I am %s years old.",this.getName(),this.getAge());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return getId() == person.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
