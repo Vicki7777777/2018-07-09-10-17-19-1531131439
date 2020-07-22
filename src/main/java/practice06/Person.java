87e8ceada3675976ec3f0888149978afb74f3d6a
VICKI PAN (ITA-ISDC-ISD-OOCLL/ZHA) <PANVI@OOCL.COM>
2020-07-22 19:30:14 +0800

    Vicki -- function submission -- complete pracce06

--- src/main/java/practice06/Person.java
+++ src/main/java/practice06/Person.java
@@ -1,4 +1,29 @@
 package practice06;
 
 public class Person {
+    protected String name;
+    protected int age;
+    public Person(String personName,int personAge){
+        this.name = personName;
+        this.age = personAge;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String introduce(){
+        return String.format("My name is %s. I am %s years old.",this.getName(),this.getAge());
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,31 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    protected Integer klass = 0;
+
+    public Teacher(String personName, int personAge,int teacherClass) {
+        super(personName, personAge);
+        this.klass = teacherClass;
+    }
+
+    public Teacher(String personName, int personAge) {
+        super(personName, personAge);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+    @Override
+    public String introduce(){
+        if(this.klass == 0){
+            return super.introduce()+String.format(" I am a Teacher. I teach No Class.");
+        }else{
+            return super.introduce()+String.format(" I am a Teacher. I teach Class %s.",this.getKlass());
+        }
+
+    }
 }
