3a03cc0e77d8ae271545fc4fd631c83b078521ea
VICKI PAN (ITA-ISDC-ISD-OOCLL/ZHA) <PANVI@OOCL.COM>
2020-07-22 15:53:20 +0800

    vicki-Completed practice02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,24 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    protected int klass;
+    public Student(String personName, int personAge,int studentClass) {
+        super(personName,studentClass);
+        this.name = personName;
+        this.age = personAge;
+        this.klass = studentClass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        String str = "I am a Student. I am at Class 2.";
+        return str;
+    }
 }
