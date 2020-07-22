e6244a2a853ed1806be2f2f16f1ada1e2a01198e
VICKI PAN (ITA-ISDC-ISD-OOCLL/ZHA) <PANVI@OOCL.COM>
2020-07-22 21:18:17 +0800

    Vicki -- function submission -- complete pracce09

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,40 @@
 package practice09;
 
-public class Teacher {
+
+
+public class Teacher extends Person {
+    protected Klass klass;
+
+    public Teacher(int id,String personName,int personAge, Klass teacherClass) {
+        super(id, personName,personAge);
+        this.klass = teacherClass;
+    }
+    public Teacher(int id,String personName,int personAge) {
+        super(id, personName,personAge);
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+
+    @Override
+    public String introduce(){
+        if(this.klass == null){
+            return super.introduce()+String.format(" I am a Teacher. I teach No Class.");
+        }else{
+            return super.introduce()+String.format(" I am a Teacher. I teach Class %s.",this.getKlass().getNumber());
+        }
+
+    }
+    public String introduceWith(Student student){
+        if(student.getKlass().getNumber() == this.klass.getNumber()){
+            return super.introduce()+String.format(" I am a Teacher. I teach %s.",student.getName());
+        }else {
+            return super.introduce() + String.format(" I am a Teacher. I don't teach %s.", student.getName());
+        }
+    }
 }
