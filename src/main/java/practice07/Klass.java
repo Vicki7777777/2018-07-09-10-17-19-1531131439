8276c276ca00b1653833e442785d02e8cd97a5d2
VICKI PAN (ITA-ISDC-ISD-OOCLL/ZHA) <PANVI@OOCL.COM>
2020-07-22 20:05:15 +0800

    Vicki -- function submission -- complete pracce07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,17 @@
 package practice07;
 
 public class Klass {
+    protected int number;
+    public Klass(int number){
+        this.number = number;
+    }
+    public int getNumber() {
+        return number;
+    }
+    public String getDisplayName(){
+        return String.format("Class %s",this.number);
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,24 @@
 package practice07;
 
-public class Student {
+
+public class Student extends Person {
+    protected Klass klass;
+    public Student(String studentName, int studentAge,Klass studentClass) {
+        super(studentName,studentAge);
+        this.name = studentName;
+        this.age = studentAge;
+        this.klass = studentClass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        return super.introduce()+String.format(" I am a Student. I am at Class %s.",this.klass.getNumber());
+    }
 }
