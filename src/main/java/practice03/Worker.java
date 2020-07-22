f3acd57d03c2c8bcdd15f515f6839d17026daa02
VICKI PAN (ITA-ISDC-ISD-OOCLL/ZHA) <PANVI@OOCL.COM>
2020-07-22 16:02:04 +0800

    vicki-Completed practices03

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,13 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person {
+
+    public Worker(String personName, int personAge) {
+        super(personName, personAge);
+    }
+
+    public String introduce(){
+        String str = "I am a Worker. I have a job.";
+        return str;
+    }
 }
