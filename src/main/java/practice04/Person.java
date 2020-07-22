99028351a331318cdc07579dde1890b7778dd76f
VICKI PAN (ITA-ISDC-ISD-OOCLL/ZHA) <PANVI@OOCL.COM>
2020-07-22 16:45:01 +0800

    vicki-Completed practice04

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,33 @@
 package practice04;
 
 public class Person {
+    protected String name;
+    protected int age;
+    public Person(String personName,int personAge){
+        this.name = personName;
+        this.age = personAge;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String introduce(){
+       return basicIntroduce();
+    }
+    public String basicIntroduce(){
+        return  "My name is "+ this.name+". I am "+ this.age+" years old.";
+    }
+
 }
--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,25 @@
 package practice04;
 
-public class Student {
+public class Student extends Person {
+    protected int klass;
+    public Student(String studentName, int studentAge,int studentClass) {
+        super(studentName,studentAge);
+        this.name = studentName;
+        this.age = studentAge;
+        this.klass = studentClass;
+    }
+
+    public int getKlass() {
+
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        return super.basicIntroduce()+" I am a Student. I am at Class "+ this.klass+".";
+    }
 }
