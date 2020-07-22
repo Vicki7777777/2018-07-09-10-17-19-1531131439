af264adde6621a71b9418da8fb0022b72426b773
VICKI PAN (ITA-ISDC-ISD-OOCLL/ZHA) <PANVI@OOCL.COM>
2020-07-22 15:32:23 +0800

    vicki-Completed practice01

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,32 @@
 package practice01;
 
 public class Person {
+    protected String name;
+    protected int age;
+    public Person(String personName,int personAge){
+        this.name = personName;
+        this.age = personAge;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String introduce(){
+        String str = "My name is Tom. I am 21 years old.";
+        return str;
+    }
+
 }
+
